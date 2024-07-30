c0dfd37c9c801c9a4aa42a12abb6ea57dfad3c78
Abdiel-88 <abdiel.f@outlook.com>
2024-07-30 18:59:08 -0400

    task 2

---
+++ csharp-hello_world/2-run_new_project.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Initialize the new project
+dotnet new console -o 2-new_project
+
+# Change directory to the new project
+cd 2-new_project
+
+# Build the project
+dotnet build
+
+# Run the project
+dotnet run
