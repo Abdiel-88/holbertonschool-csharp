6f3125afbce431e0ee08e735230c69b279452c88
Abdiel Feliciano <abdiel.f@outlook.com>
2024-07-31 02:19:19 -0400

    task 7 concat

---
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = "Welcome to";
+        string str2 = "Holberton School!";
+        str1 = str1 + " " + str2;
+        Console.WriteLine(str1);
+    }
+}
