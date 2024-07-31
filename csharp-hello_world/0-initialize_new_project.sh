9d02ac9df8c8897d2b1f49637b134883ec587d79
Abdiel Feliciano <abdiel.f@outlook.com>
2024-07-30 20:02:48 -0400

    task 0

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-# Create a new directory for the project
-mkdir -p 0-new_project
-
-# Navigate into the project directory
-cd 0-new_project
-
-# Initialize a new C# console project
-dotnet new console
-
-# Rename the Program.cs file as required by your task guidelines
-mv Program.cs 0-new_project.cs
-
-# Navigate back to the original directory
-cd ..
-
-# Output success message
-echo "The template 'Console Application' was created successfully."
+if [ ! -d "0-new_project" ]; then
+    dotnet new console -n 0-new_project
+fi
