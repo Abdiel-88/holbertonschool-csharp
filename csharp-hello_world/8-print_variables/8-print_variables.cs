432f47362ffb478937a1e6f7e44cfda91e7c3252
Abdiel Feliciano <abdiel.f@outlook.com>
2024-07-31 02:27:49 -0400

    task 8 print variables

--- csharp-hello_world/8-print_variables/8-print_variables.cs
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿﻿using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        int number = 972;
-        string street = "Mission Street";
-        Console.WriteLine("{0} {1}", number, street);
+        string str1 = "Street";
+        string str2 = "Mission";
+        int int1 = 972;
+        Console.WriteLine("{2} {1} {0}", str1, str2, int1);
     }
 }
