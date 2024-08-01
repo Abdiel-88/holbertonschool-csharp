fe3ad3e935f3180281cbaf93a73e68fca1cf992b
Abdiel Feliciano <abdiel.f@outlook.com>
2024-08-01 16:15:13 -0400

    task 7

--- csharp-arrays_lists_dictionaries/7-delete_at/obj/Debug/net6.0/7-delete_at.AssemblyInfo.cs
+++ csharp-arrays_lists_dictionaries/7-delete_at/obj/Debug/net6.0/7-delete_at.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("7-delete_at")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4153c2ca637666bc10befbf26e89ff71f46553cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b40a0da22b109053da194a0ef3792bf89a04833a")]
 [assembly: System.Reflection.AssemblyProductAttribute("7-delete_at")]
 [assembly: System.Reflection.AssemblyTitleAttribute("7-delete_at")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
