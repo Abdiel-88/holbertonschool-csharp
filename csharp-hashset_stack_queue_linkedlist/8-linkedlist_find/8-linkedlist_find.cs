2619bcf54ab56f00729d1b9fc922775cb7e5d618
Abdiel Feliciano <abdiel.f@outlook.com>
2024-08-05 17:24:24 -0400

    task 8

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,20 @@
+﻿// 8-linkedlist_find.cs
+using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int index = 0;
+        foreach (int item in myLList)
+        {
+            if (item == value)
+            {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+}
