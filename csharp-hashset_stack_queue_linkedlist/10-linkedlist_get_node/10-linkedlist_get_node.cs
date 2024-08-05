143e613cdc682d5877993fc940e8b17fa95746e8
Abdiel Feliciano <abdiel.f@outlook.com>
2024-08-05 17:36:19 -0400

    task 10

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,24 @@
+﻿// 10-linkedlist_get_node.cs
+using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (n < 0 || n >= myLList.Count)
+        {
+            return 0;
+        }
+        int index = 0;
+        foreach (int value in myLList)
+        {
+            if (index == n)
+            {
+                return value;
+            }
+            index++;
+        }
+        return 0;
+    }
+}
