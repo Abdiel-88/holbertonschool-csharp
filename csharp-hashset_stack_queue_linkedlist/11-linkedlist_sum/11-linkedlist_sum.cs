5d2fb6d3f4fddc273cb1999e49554df2b01fb44a
Abdiel Feliciano <abdiel.f@outlook.com>
2024-08-05 17:42:01 -0400

    task 11

---
+++ csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,16 @@
+﻿// 11-linkedlist_sum.cs
+using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int sum = 0;
+        foreach (int value in myLList)
+        {
+            sum += value;
+        }
+        return sum;
+    }
+}
