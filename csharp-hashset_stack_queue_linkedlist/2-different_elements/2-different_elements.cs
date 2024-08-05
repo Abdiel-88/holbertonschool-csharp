2db4e413684b90eb9df1f979a9a1628a588a96aa
Abdiel Feliciano <abdiel.f@outlook.com>
2024-08-05 14:08:01 -0400

    task 2

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,32 @@
+﻿// 2-different_elements.cs
+using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> set1 = new HashSet<int>(list1);
+        HashSet<int> set2 = new HashSet<int>(list2);
+        List<int> differentElements = new List<int>();
+
+        foreach (int element in set1)
+        {
+            if (!set2.Contains(element))
+            {
+                differentElements.Add(element);
+            }
+        }
+
+        foreach (int element in set2)
+        {
+            if (!set1.Contains(element))
+            {
+                differentElements.Add(element);
+            }
+        }
+
+        differentElements.Sort();
+        return differentElements;
+    }
+}
