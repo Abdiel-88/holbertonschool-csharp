26f611a178714d3cc5c4797558ae74749b48a935
Abdiel Feliciano <abdiel.f@outlook.com>
2024-08-05 17:51:30 -0400

    task 13

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,25 @@
+﻿// 13-linkedlist_delete.cs
+using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        if (index < 0 || index >= myLList.Count)
+        {
+            return;
+        }
+
+        LinkedListNode<int> current = myLList.First;
+        for (int i = 0; i < index; i++)
+        {
+            current = current.Next;
+        }
+
+        if (current != null)
+        {
+            myLList.Remove(current);
+        }
+    }
+}
