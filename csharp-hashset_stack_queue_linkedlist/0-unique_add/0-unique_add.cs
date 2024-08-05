7e921dd2d26ce994e3067ea5391b1f2582cf47e5
Abdiel Feliciano <abdiel.f@outlook.com>
2024-08-05 13:51:09 -0400

    task 0

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,17 @@
+﻿// 0-unique_add.cs
+using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> uniqueNumbers = new HashSet<int>(myList);
+        int sum = 0;
+        foreach (int number in uniqueNumbers)
+        {
+            sum += number;
+        }
+        return sum;
+    }
+}
