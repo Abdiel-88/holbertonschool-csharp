0c7bd3c595df749b204010f9e367f76ab140b6e4
Abdiel Feliciano <abdiel.f@outlook.com>
2024-08-19 12:56:04 -0400

    task 1 divide

---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        try
+        {
+            int result = a / b;
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine($"{a} / {b} = {(b != 0 ? a / b : 0)}");
+        }
+    }
+}
