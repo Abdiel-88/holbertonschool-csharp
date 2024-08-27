3207ef7eee8a0c8ee42ce3a53125b7a8b0039a7e
Abdiel Feliciano <abdiel.f@outlook.com>
2024-08-27 12:51:14 -0400

    task 1

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a Zombie.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Health of the Zombie.
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class and sets health to 0.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
