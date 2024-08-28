75ad084c04253c1a18e77141b0b7cc74634bf8fe
Abdiel Feliciano <abdiel.f@outlook.com>
2024-08-28 11:29:28 -0400

    created new dir and added readme

--- csharp-hello_world/0-new_project/obj/Debug/netcoreapp2.1/0-new_project.AssemblyInfo.cs
+++ csharp-hello_world/0-new_project/obj/Debug/netcoreapp2.1/0-new_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("0-new_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bb76fb0e610732da79b9f5f57d7e72419b2293d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17f559742760d582711fdc66fc80e53b62ee94d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("0-new_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("0-new_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
