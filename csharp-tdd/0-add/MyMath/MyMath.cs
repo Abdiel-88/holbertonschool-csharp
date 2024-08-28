f9b36cc1f6cce77dc7e8825fa4f4f90f35595092
Abdiel Feliciano <abdiel.f@outlook.com>
2024-08-28 11:47:18 -0400

    no shot this works, task 0

---
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>
+    /// Contains operations for basic mathematical functions.
+    /// </summary>
+    public static class Operations
+    {
+        /// <summary>
+        /// Adds two integers and returns the result.
+        /// </summary>
+        /// <param name="a">First integer</param>
+        /// <param name="b">Second integer</param>
+        /// <returns>The sum of the two integers</returns>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
