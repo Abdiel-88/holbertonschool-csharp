871b9184e0a4df77d189a76d826cf609e25f591c
Abdiel Feliciano <abdiel.f@outlook.com>
2024-09-17 14:34:23 -0400

    task 18 fix

--- csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -1,40 +1,37 @@
 using System;
 
-/// <sumary>
-/// method that multiplies two matrices,
-/// and returns the resulting matrix.
-/// </sumary>
 class MatrixMath
 {
-    /// <sumary>
-    /// The matrices will not necessarily
-    /// be square or the same dimensions
-    /// If the matrices cannot be multiplied,
-    /// </sumary>
-    /// <return>
-    /// return a matrix containing -1
-    /// </return>
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
-        double[,] mult_matrix = new double[matrix2.GetLength(0), matrix2.GetLength(1)];
+        // Get the dimensions of both matrices
+        int rowsMatrix1 = matrix1.GetLength(0);
+        int colsMatrix1 = matrix1.GetLength(1);
+        int rowsMatrix2 = matrix2.GetLength(0);
+        int colsMatrix2 = matrix2.GetLength(1);
 
-        if (matrix1.GetLength(1) != matrix2.GetLength(0))
+        // Check if the matrices can be multiplied (cols of matrix1 == rows of matrix2)
+        if (colsMatrix1 != rowsMatrix2)
         {
-            return new double[,] {{-1}};
+            return new double[,] { { -1 } };
         }
 
-        for (int i = 0; i < mult_matrix.GetLength(0); i++)
+        // Initialize the result matrix
+        double[,] result = new double[rowsMatrix1, colsMatrix2];
+
+        // Perform matrix multiplication
+        for (int i = 0; i < rowsMatrix1; i++)
         {
-            for (int j = 0; j < mult_matrix.GetLength(1); j++)
+            for (int j = 0; j < colsMatrix2; j++)
             {
-                double temp = 0;
-                for (int k = 0; k < mult_matrix.GetLength(0); k++)
+                result[i, j] = 0;
+                for (int k = 0; k < colsMatrix1; k++)
                 {
-                    temp += matrix1[i,k] * matrix2[k,j];
+                    result[i, j] += matrix1[i, k] * matrix2[k, j];
                 }
-                mult_matrix[i,j] = Math.Round(temp, 2);
             }
         }
-        return mult_matrix;
+
+        return result;
     }
 }
